9d3fe1d9bde01fe9d226d631959d05ba5a1064f5
SushantNath <sushbhubaneswar1@gmail.com>
2021-07-11 16:28:43 +0530

    updatedcode11/07

--- webapp/controller/homeView.controller.js
+++ webapp/controller/homeView.controller.js
@@ -1,9 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function(Controller,MessageBox) {
 	"use strict";
 
 	return Controller.extend("com.sap.huCreationinboundHu.controller.homeView", {
+	onInit: function () {
+		
+	},
+
+	onApplyFilter: function (e) {
+		        var inboundDelivery = this.getView().byId("inboundId").getValue();
+				var asnNumber = this.getView().byId("asnNumberrId").getValue();
+				var materialCode = this.getView().byId("materialCodeId").getValue();
+				var batchnumber = this.getView().byId("batchNumberId").getValue();
+				var huValue = this.getView().byId("huId").getSelected();
+				
+				if (inboundDelivery=== "" && asnNumber===""){
+					
+					MessageBox.error("Please enter either inbound delivery# or asn# to proceed");
+					
+				}
+		
+	}
+
 
 	});
 });
